a1728ba87229c3d516b83bdd9c1d68caa69217b4
Austin Fell <austinrf2918@yahoo.com>
2016-04-02 17:43:01 -0400

    Created fairly robust system to parsing attributes, still needs implementation

--- stuff.py
+++ stuff.py
@@ -1,14 +1,36 @@
 import requests
 import json
 
-dataOne = '["StartBootstrap-OnePagePortfolio", {"Attributes": ["one page", "business", "beautiful"]}]'
-dataTwo = '["StartBootstrap-TwoPagePortfolio", {"Attributes": ["two page", "personal", "drab"]}]'
+searchTermDict = {}
+searchTermList = []
+
+fileList = []
+
+with open("keywords.json") as objectAttributes:
+    data = json.load(objectAttributes)
+
+with open("fileAttributes.json") as localDatabase:
+    databaseInfo = json.load(localDatabase)
+
+
+print("Pre Implementation of JSON management tools for Szism")
+print("By: Austin Fell")
+print("(Non-Release)")
 
 if __name__ == "__main__":
-    loadedJSON = []
-    loadedJSON.append(json.loads(dataOne))
-    loadedJSON.append(json.loads(dataTwo))
-    temp = loadedJSON[0]
-    for i in temp[1]['Attributes']:
-        print(i)
+    for i in data["SearchTerms"][0]:
+        j = data["SearchTerms"][0][i]
+        searchTermDict[i] = []
+        searchTermList.append(i)
+        for z in j:
+            searchTermDict[i].append(z)
+
+print("Now reading in partially corrupt data from local database")
+
+for i in databaseInfo:
+    for j in searchTermList:
+        try:
+            print("Parsing " + i + " " + j + ": " + databaseInfo[i][0][j])
+        except KeyError:
+            print("KEY " + j + " DOES NOT EXIST. PLEASE CHECK COMPOSITION OF fileAttributes.json")
 
