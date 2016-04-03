b312c1828dceb2a0e2604041a27769ce873cf49c
Austin Fell <austinrf2918@yahoo.com>
2016-04-03 00:40:10 -0400

    Added full fledged class for parsing

--- stuff.py
+++ stuff.py
@@ -1,36 +1,73 @@
 import requests
 import json
 
-searchTermDict = {}
-searchTermList = []
+class jsonParser:
+    searchTermDict = {}
+    searchTermList = []
 
-fileList = []
+    fileList = []
+    def __init__(self, keywordsJSON, localDatabaseJSON, debugMode):
+        with open(keywordsJSON) as objectAttributes:
+            self.data = json.load(objectAttributes)
+        with open(localDatabaseJSON) as localDatabase:
+            self.databaseInfo = json.load(localDatabase)
 
-with open("keywords.json") as objectAttributes:
-    data = json.load(objectAttributes)
+        for i in self.data["SearchTerms"][0]:
+            j = self.data["SearchTerms"][0][i]
+            self.searchTermDict[i] = []
+            self.searchTermList.append(i)
+            for z in j:
+                self.searchTermDict[i].append(z)
 
-with open("fileAttributes.json") as localDatabase:
-    databaseInfo = json.load(localDatabase)
+        if debugMode is True:
+            print(self.searchTermDict)
+
+    def showParsed(self):
+        for i in self.databaseInfo:
+            for j in self.searchTermList:
+                try:
+                    print("Parsing " + i + " " + j + ": " + self.databaseInfo[i][0][j])
+                except KeyError:
+                    print("Key " + j + " does not exist. please check composition of fileAttributes.json")
+
+    def printSearchTerms(self):
+        print(self.searchTermList)
+
+    def findWithAtrributes(self, attributeList):
+        tempDict = {}
+        tempDict2 = {}
+
+        for i in self.databaseInfo:
+            tempDict[i] = self.databaseInfo[i]
+            tempDict2[i] = self.databaseInfo[i]
+
+        tempKeys = tempDict.keys()
+
+        for i in attributeList:
+            for j in i.keys():
+                if j not in self.searchTermList:
+                    print("You searched for a term that was not in the term list.")
+                    break
+
+        for i in tempKeys: #go through all our possible files
+            for j in attributeList: #go through all the entered data
+                for z in j.keys():
+                    if j[z] != tempDict[i][0][z]:
+                        if i in tempDict2:
+                            del tempDict2[i]
+
+        return tempDict2
+
+    def scrapeKeys(self, fwaObject):
+        tempKeys = []
+        for i in fwaObject.keys():
+            tempKeys.append(i)
+        return tempKeys
 
 
 print("Pre Implementation of JSON management tools for Szism")
 print("By: Austin Fell")
-print("(Non-Release)")
 
-if __name__ == "__main__":
-    for i in data["SearchTerms"][0]:
-        j = data["SearchTerms"][0][i]
-        searchTermDict[i] = []
-        searchTermList.append(i)
-        for z in j:
-            searchTermDict[i].append(z)
-
-print("Now reading in partially corrupt data from local database")
-
-for i in databaseInfo:
-    for j in searchTermList:
-        try:
-            print("Parsing " + i + " " + j + ": " + databaseInfo[i][0][j])
-        except KeyError:
-            print("KEY " + j + " DOES NOT EXIST. PLEASE CHECK COMPOSITION OF fileAttributes.json")
-
+dataObject = jsonParser("keywords.json", "fileAttributes.json", False)
+z = dataObject.findWithAtrributes([{"SinglePage" : "Multi Page"}, {"Beautiful" : "No"}])
+print(dataObject.scrapeKeys(z))
